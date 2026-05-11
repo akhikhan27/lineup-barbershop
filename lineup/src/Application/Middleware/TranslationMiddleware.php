c1067e769ec80cdae043c35a317bfa006e0525f1
akhikhan27 <madyank007@gmail.com>
2026-05-11 11:23:04 -0400

    added language toggle

--- lineup/src/Application/Middleware/TranslationMiddleware.php
+++ lineup/src/Application/Middleware/TranslationMiddleware.php
@@ -6,15 +6,18 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\MiddlewareInterface as Middleware;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+use Slim\Views\Twig;
 use Symfony\Component\Translation\Translator;
 
 class TranslationMiddleware implements Middleware
 {
     private Translator $translator;
+    private Twig $twig;
 
-    public function __construct(Translator $translator)
+    public function __construct(Translator $translator, Twig $twig)
     {
         $this->translator = $translator;
+        $this->twig = $twig;
     }
 
     public function process(Request $request, RequestHandler $handler): Response
@@ -23,6 +26,7 @@ class TranslationMiddleware implements Middleware
         $lang = in_array($lang, ['en', 'fr']) ? $lang : 'en';
         $_SESSION['lang'] = $lang;
         $this->translator->setLocale($lang);
+        $this->twig->getEnvironment()->addGlobal('session', $_SESSION);
         return $handler->handle($request);
     }
 }
